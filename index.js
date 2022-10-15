dbcd0489132cb3e56e9eab61242ff8ba32ab33a6
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 23:18:17 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = "Игорь."
+const programmingLanguage = "Java Script"
+const courseCreatorName = "Владилена Минина."
+const reasonText = "давно хотел научится создавать сайты."
+const numberOfMonth = "0"
+
+console.log("Всем привет! Меня зовут", `${myName}`, "Сейчас я изучаю язык программирования", `${programmingLanguage}`, "на курсе по", `${programmingLanguage}`, "у", `${courseCreatorName}`, "Я хочу стать веб-разработчиком, потому что", `${reasonText}`, "До этого я изучал", `${programmingLanguage}`, `${numberOfMonth}`, "мксяцев.", "Я уверен, что пройду данный курс до конца!")
